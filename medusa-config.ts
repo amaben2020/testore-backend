b31009d62a1df3b02bf4241b0c4041e94cf8a1ce
amaben2020 <uzochukwubennethamara@gmail.com>
2025-01-28 14:38:57 +0100

    build fix

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,19 +2,6 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils';
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
-const plugins = [
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    },
-  },
-  {
-    resolve: 'medusa-plugin-wishlist',
-  },
-];
-
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -25,7 +12,17 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
     },
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | 'shared'
+      | 'worker'
+      | 'server',
+
+    redisUrl: process.env.REDIS_URL,
   },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
+  },
+
   plugins: [
     {
       resolve: '@medusajs/payment',
@@ -47,6 +44,28 @@ module.exports = defineConfig({
             public_key: process.env.PAYSTACK_PUBLIC_KEY,
           },
         ],
+      },
+    },
+  ],
+  modules: [
+    {
+      resolve: '@medusajs/medusa/cache-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: '@medusajs/medusa/event-bus-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: '@medusajs/medusa/workflow-engine-redis',
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
       },
     },
   ],
